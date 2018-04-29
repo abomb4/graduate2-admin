0a56b96faf0b429b903b14cd7432b35692c235a8
abomb4 <yangrlx@gmail.com>
2018-04-29 22:08:44 +0800

    eslint errors

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './LoginPage.css';
-import { ItrsApi } from '../../api/ItrsApi';
 import { userActions } from '../../_actions';
-
 
 class LoginPage extends Component {
   constructor(props) {
